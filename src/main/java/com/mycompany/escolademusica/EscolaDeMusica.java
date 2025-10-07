239473555c1339cbc7e7a9e1e7619a815914a831
LARA GOMES PAIVA <lara.gpaiva@a.ucb.br>
2025-10-07 10:02:08 -0300

    1

---
+++ src/main/java/com/mycompany/escolademusica/EscolaDeMusica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escolademusica;
+
+/**
+ *
+ * @author lara.gpaiva
+ */
+public class EscolaDeMusica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
